69c44541d7f78542d5043aeae31ef35164b7dd97
Harsh Asnade <harshasnade24@gmail.com>
2025-07-09 17:07:00 +0000

    make toggle efect in dark - light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased transition-colors duration-300">
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
-            disableTransitionOnChange
+            disableTransitionOnChange={false}
         >
             <UserProvider>
             {children}
